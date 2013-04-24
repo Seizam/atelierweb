623fbe0bd5e7b0b12a1217bb3e722bbedae2bf46
Yannouk <yann@seizam.com>
2013-04-24 17:37:41 +0200

    fixes phone validation of contact form

--- wp-content/plugins/contact-form-7/includes/formatting.php
+++ wp-content/plugins/contact-form-7/includes/formatting.php
@@ -129,7 +129,7 @@ function wpcf7_is_url( $url ) {
 }
 
 function wpcf7_is_tel( $tel ) {
-	$result = preg_match( '/^[0-9+-\s]*$/', $tel );
+	$result = preg_match( '/^[0-9+-\s\(\)]*$/', $tel );
 	return apply_filters( 'wpcf7_is_tel', $result, $tel );
 }
 
